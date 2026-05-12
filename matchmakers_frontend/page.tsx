134bf31b1625b369b5745f63423847a60a93cc98
Shreejal Shah <shreejalshah21@gmail.com>
2026-05-12 08:30:52 +0545

    Add files via upload
    Signed-off-by: Shreejal Shah <shreejalshah21@gmail.com>

---
+++ matchmakers_frontend/page.tsx
@@ -0,0 +1,12 @@
+import { text } from "node:stream/consumers";
+
+export default function HelloWorld() {
+  return (
+    <div className="sex">
+      <h1 class={'text-red-500'}>Hello World</h1>
+      <h2>fuck you</h2>
+      <p class="text-blue-500">This is blue text.</p>
+
+    </div>
+  )
+}
